979e0b6b9b17eacb824cda524c097c3a51e410ee
kakerusaman <kakerusaman@gmail.com>
2026-03-08 00:16:42 +0900

    データベースの向き先が違っていたため修正Eclipseでは動いていたが、、、

--- Java-Practice/src/main/java/com/example/config/AppConfig.java
+++ Java-Practice/src/main/java/com/example/config/AppConfig.java
@@ -26,7 +26,7 @@ public class AppConfig {
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl("jdbc:postgresql://localhost:5432/javapractice");
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
 		dataSource.setUsername("postgres");
 		dataSource.setPassword("yourpassword");
 		return dataSource;
