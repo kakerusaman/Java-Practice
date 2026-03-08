b842b1fc8cca951bf92a61de3bb5581eb15c8c7e
kakerusaman <kakerusaman@gmail.com>
2026-03-08 22:31:49 +0900

    テスト用

--- Java-Practice/src/main/java/com/example/controller/LoginController.java
+++ Java-Practice/src/main/java/com/example/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,6 +30,10 @@ public class LoginController {
         //エラーだったらログイン画面に遷移
 
         UserData userdata = userService.checkLoginData(form.getUserId(),form.getPassword());
+        List<UserData> userfindAllData = userService.findAll();
+        if (userfindAllData != null){
+            System.out.println("konnnitiha");
+        }
         if (userdata != null){
             System.out.println("syoriseikou");
         } else {
