7453ad312ddc8d51a31de2ae1dcca7fc9d82e6a9
kakerusaman <kakerusaman@gmail.com>
2026-03-10 23:47:37 +0900

    コメント追加

--- Java-Practice/src/main/java/com/example/controller/RegisterController.java
+++ Java-Practice/src/main/java/com/example/controller/RegisterController.java
@@ -2,9 +2,7 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import com.example.form.UserDataForm;
 
 
@@ -18,7 +16,7 @@ public class RegisterController {
 
     @PostMapping("/register")
     public String postMethodName(UserDataForm form) {
-        //TODO: process POST request
+        //登録処理
         
         return "registerComplete";
     }
