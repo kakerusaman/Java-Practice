069132737612a5fe9ecdc453f2d5b5738d5051dd
kakerusaman <kakerusaman@gmail.com>
2026-03-11 22:48:24 +0900

    user登録できるように変更

--- Java-Practice/src/main/java/com/example/mapper/UserMapper.java
+++ Java-Practice/src/main/java/com/example/mapper/UserMapper.java
@@ -10,7 +10,12 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface UserMapper {
 
-    UserData findByLoginIdAndPassword(@Param("userId") String userId, @Param("password")String password);
+    // ログインMapper
+    UserData findByLoginIdAndPassword(@Param("userId") String userId, @Param("password") String password);
 
+    // 今の所使い道なし
     List<UserData> findAll();
+
+    // user登録Mapper
+    void insertUser(@Param("userId") String userId, @Param("password") String password);
 }
--- Java-Practice/src/main/java/com/example/service/UserService.java
+++ Java-Practice/src/main/java/com/example/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.mapper.UserMapper;
 import com.example.model.UserData;
@@ -13,11 +14,20 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public UserData checkLoginData(String userId, String password){
         return userMapper.findByLoginIdAndPassword(userId, password);
     }
 
     public List<UserData> findAll(){
         return userMapper.findAll();
+    }
+
+    public void insertUser(String userId, String password){
+        String hashPass = passwordEncoder.encode(password);
+
+        userMapper.insertUser(userId, hashPass);
     }
 }
