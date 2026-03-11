a152102c522cca526061b3f06b83f730805be304
kakerusaman <kakerusaman@gmail.com>
2026-03-11 22:48:09 +0900

    試しにハッシュか

--- Java-Practice/src/main/java/com/example/controller/RegisterController.java
+++ Java-Practice/src/main/java/com/example/controller/RegisterController.java
@@ -1,13 +1,18 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.example.form.UserDataForm;
+import com.example.service.UserService;
 
 
 @Controller
 public class RegisterController {
+
+    @Autowired
+    UserService userService;
     
     @GetMapping("/register")
     public String register() {
@@ -17,6 +22,7 @@ public class RegisterController {
     @PostMapping("/register")
     public String postMethodName(UserDataForm form) {
         //登録処理
+        userService.insertUser(form.getUserId(), form.getPassword());
         
         return "registerComplete";
     }
