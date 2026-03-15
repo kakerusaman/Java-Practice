64cc387327da79593d329cd89f44e57733def73d
kakerusaman <kakerusaman@gmail.com>
2026-03-15 11:18:44 +0900

    外部APIを呼ぶ用の画面を追加何を呼ぶかは考えないといけない

---
+++ Java-Practice/src/main/java/com/example/controller/ExternalController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@Controller
+public class ExternalController {
+    
+    @GetMapping("external")
+    public String external() {
+        return new String();
+    }
+
+    // 天気APIを呼び出す（最初は下手書きにしてるけどのちに環境変数に移す予定）
+    @GetMapping("weather")
+    public String weather() {
+        // 天気API無料枠を調べられなかったからまた今度にする
+        return "hoge";
+    }
+    
+    
+}
