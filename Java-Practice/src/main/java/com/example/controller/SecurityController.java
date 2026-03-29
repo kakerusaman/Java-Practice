b975d394b48bcb26994cdec76762ccdb2999bc7b
kakerusaman <kakerusaman@gmail.com>
2026-03-29 22:26:37 +0900

    セキュリティ選択画面へ遷移するためのコントローラー

--- Java-Practice/src/main/java/com/example/controller/SecurityController.java
+++ Java-Practice/src/main/java/com/example/controller/SecurityController.java
@@ -1,8 +1,19 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class SecurityController {
+    
+    /*
+    セキュリティ選択画面に遷移
+    */
+    @GetMapping("security")
+    public String security(@RequestParam String param) {
+        return "security/securityMenu";
+    }
     
 }
