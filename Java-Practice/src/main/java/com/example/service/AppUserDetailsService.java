b7ad9612403a94a7040ff3b632293319d764436e
kakerusaman <kakerusaman@gmail.com>
2026-03-29 20:46:47 +0900

    ROLEをDBについ明かしていないため固定でROLEを設定している

--- Java-Practice/src/main/java/com/example/service/AppUserDetailsService.java
+++ Java-Practice/src/main/java/com/example/service/AppUserDetailsService.java
@@ -1,5 +1,30 @@
 package com.example.service;
 
-public class AppUserDetailsService {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.mapper.AppUserMapper;
+import com.example.model.UserData;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService{
+
+    @Autowired
+    AppUserMapper appUserMapper;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		UserData user = appUserMapper.findByUserName(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+		return User.withUsername(user.getUserId())
+				.password(user.getPassword())
+				.roles("USER")
+				.build();
+	}
 
 }
