73559e85b1483c909c19e67c6b8cf2016e3c699a
kakerusaman <kakerusaman@gmail.com>
2026-04-01 23:47:35 +0900

    試験的にすべてNotnullアノテーション

--- Java-SpringSecurity/src/main/java/com/example/form/UserForm.java
+++ Java-SpringSecurity/src/main/java/com/example/form/UserForm.java
@@ -1,5 +1,6 @@
 package com.example.form;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,15 +9,19 @@ import lombok.Setter;
 public class UserForm {
 
     // 画面から入力してもらう
+    @NotNull
     private String userId;
 
     //画面から入力してもらう
+    @NotNull
     private String password;
 
     //画面から入力してもらう
+    @NotNull
     private String email;
 
     //画面から入力してもらう
+    @NotNull
     private String role;
 
 }
