777104717cca4bd348170f539bde40849702d46c
kakerusaman <kakerusaman@gmail.com>
2026-04-04 22:54:23 +0900

    Modelを作成

--- Java-SpringSecurity/src/main/java/com/example/model/UserData.java
+++ Java-SpringSecurity/src/main/java/com/example/model/UserData.java
@@ -6,6 +6,22 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserData {
-    private String userId;
+    // ユーザーID
+    private String id;
+    // ログインID
+    private String loginId;
+    // パスワード
     private String password;
+    // メールアドレス
+    private String email;
+    // ロール
+    private String role;
+    // 認証失敗回数
+    private int loginFailureCount;
+    // アカウント削除フラグ
+    private boolean isDeleted;
+    // アカウント作成日
+    private String createdAt;
+    // 最終更新時間
+    private String updatedAt;
 }
