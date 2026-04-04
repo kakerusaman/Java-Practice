b4406d6f0bdc571bf85a2cf4c5e6ed5026df43a2
kakerusaman <kakerusaman@gmail.com>
2026-04-04 22:54:06 +0900

    DB内のみ取得する値を追加

--- Java-SpringSecurity/src/main/java/com/example/form/UserForm.java
+++ Java-SpringSecurity/src/main/java/com/example/form/UserForm.java
@@ -1,5 +1,7 @@
 package com.example.form;
 
+import java.time.LocalDateTime;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +9,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserForm {
+
+    // ユーザーID
+    private String id;
 
     // 画面から入力してもらう
     @NotNull
@@ -23,5 +28,17 @@ public class UserForm {
     //画面から入力してもらう
     @NotNull
     private String role;
+
+    // ログインを間違えた回数
+    private int loginFailureCount;
+
+    // 削除フラグ
+    private boolean isDeleted;
+
+    // アカウント作成時間
+    private LocalDateTime createdAt;
+
+    // 最終更新時間
+    private LocalDateTime updatedAt;
 
 }
