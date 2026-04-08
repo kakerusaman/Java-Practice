bee198714bfe4b857a23daf028863df6385218cf
kakerusaman <kakerusaman@gmail.com>
2026-04-09 07:53:19 +0900

    認証処理を記述

--- Java-SpringSecurity/src/main/java/com/example/service/LoginUserDetailsService.java
+++ Java-SpringSecurity/src/main/java/com/example/service/LoginUserDetailsService.java
@@ -1,18 +1,29 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.example.mapper.LoginUserMapper;
 import com.example.model.UserData;
 
 @Service
 public class LoginUserDetailsService implements UserDetailsService {
     
+    @Autowired
+    LoginUserMapper loginUserMapper;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserData user = 
+    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+        UserData user = loginUserMapper.findByLoginName(loginId)
+                .orElseThrow(() -> new UsernameNotFoundException("notUser"));
+            
+        return User.withUsername(user.getLoginId())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
     }
 }
