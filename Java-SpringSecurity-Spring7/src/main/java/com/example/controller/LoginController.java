aa17da7f05b5da83e78164501f3a379b03839551
kakerusaman <kakerusaman@gmail.com>
2026-04-18 22:51:13 +0900

    リダイレクトは多分いらないけど一応残しています。

--- Java-SpringSecurity-Spring7/src/main/java/com/example/controller/LoginController.java
+++ Java-SpringSecurity-Spring7/src/main/java/com/example/controller/LoginController.java
@@ -10,6 +10,11 @@ public class LoginController {
     POSTはSpringSecurityが自動的に行ってくれるため実装なし
     */
 
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/complete";
+    }
+
     @GetMapping("/login")
     public String login() {
         return "login";
